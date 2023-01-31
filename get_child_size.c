f4f62e63db0b61c70d75923f8ac0f1e035017865
Ssuamje <side2850@naver.com>
2023-01-31 20:17:18 +0900

    added get_child_size

--- get_child_size.c
+++ get_child_size.c
@@ -6,11 +6,11 @@
 /*   By: sanan <sanan@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 16:12:40 by hyungnoh          #+#    #+#             */
-/*   Updated: 2023/01/31 20:13:10 by sanan            ###   ########.fr       */
+/*   Updated: 2023/01/31 20:14:49 by sanan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "/Users/ssuamje/minishell/include/minishell.h"
+#include "minishell.h"
 
 int	get_child_size(t_proc *proc)
 {
