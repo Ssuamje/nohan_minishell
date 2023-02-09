7b6efb90f560cf0c8cef88f27c33d7a8d5feee6a
Sangje An <sanan@c2r6s4.42seoul.kr>
2023-02-09 10:56:32 +0900

    export ~해석되서 들어왔을때 '/' 특수문자 못거르는거 수정

--- builtin/export/export.c
+++ builtin/export/export.c
@@ -6,7 +6,7 @@
 /*   By: sanan <sanan@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/02 21:26:02 by sanan             #+#    #+#             */
-/*   Updated: 2023/02/08 13:13:44 by sanan            ###   ########.fr       */
+/*   Updated: 2023/02/09 10:56:06 by sanan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	is_export_syntax_error(char *cmd)
 {
 	return ((ft_strcmp(cmd, "") == TRUE) \
 	|| (ft_isdigit(cmd[0]) == TRUE) \
-	|| (is_in_charset(cmd[0], "`~!@#%$%^&*(){}\\|-\'\";:+,.=?")) \
+	|| (is_in_charset(cmd[0], "`~!@#%$%^&*(){}\\|-\'\";:+,.=?/")) \
 	|| (is_cmd_has_special_after(cmd)));
 }
 
